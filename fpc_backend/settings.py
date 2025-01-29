a0561cb6ea277cd1959f5c3d8d49367bc5c31f9a
Bright Sefah <sefahb13@gmail.com>
2025-01-29 04:20:49 +0000

    Refix deploment Errors--4: STATIC SETTINGS

--- fpc_backend/settings.py
+++ fpc_backend/settings.py
@@ -133,8 +133,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static/')
+STATIC_DIR = (
+    os.path.join(PROJECT_ROOT, 'static')
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
